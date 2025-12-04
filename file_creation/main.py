f3de815ccaae61ac40e670e056c6655c93a76d29
aftab780879 <aftabalam780879@gmail.com>
2025-12-04 18:19:49 +0530

    File Creation

---
+++ file_creation/main.py
@@ -0,0 +1,24 @@
+import os
+import time
+
+folder_path=os.path.join(os.getcwd(),"file_creation")
+
+
+file_number=input("Please enter the number of files you want to create: ")
+if file_number.isdigit():
+    file_number=int(file_number)
+
+    for number in range(file_number):
+        file_name=input(f"\nPlease enter the file {number+1} name: ").lower()
+        
+        file_path=os.path.join(folder_path,file_name)
+        with open(file_path,'w') as file:
+            file.write("My name is aftab")
+        print("\nCreating.............")
+        time.sleep(2)
+        print(f"\nYour file name {file_name}.txt created Successfully")
+
+    print("\nAll Files Created Successfully")        
+else:
+    print("Invalid Input , PLease use Numbers")            
+
