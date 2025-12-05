d1b49c62144aff2aad872bda2fb9bfca93815427
aftab780879 <aftabalam780879@gmail.com>
2025-12-05 10:48:15 +0530

    updated

--- file_creation/main.py
+++ file_creation/main.py
@@ -9,14 +9,14 @@ if file_number.isdigit():
     file_number=int(file_number)
 
     for number in range(file_number):
-        file_name=input(f"\nPlease enter the file {number+1} name: ").lower()
+        file_name=input(f"\nPlease enter the file {number+1} name: ").lower()+(".txt")
         
         file_path=os.path.join(folder_path,file_name)
         with open(file_path,'w') as file:
             file.write("My name is aftab")
         print("\nCreating.............")
         time.sleep(2)
-        print(f"\nYour file name {file_name}.txt created Successfully")
+        print(f"\nYour file name {file_name} created Successfully")
 
     print("\nAll Files Created Successfully")        
 else:
